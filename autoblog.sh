bf7d835d240cb41570a01ca5fac9bbb20f3a377c
Michele Sciabarra <michele@sciabarra.com>
2020-07-25 19:23:36 +0200

    fixed autoblog

--- autoblog.sh
+++ autoblog.sh
@@ -2,10 +2,12 @@
 cd "$(dirname $0)"
 # category Blog
 C=16
-mkdir -p content/post/forum
 for id in $(curl -sL https://noiopen.discourse.group/c/$C.json | jq .topic_list.topics[].id)
 do 
-   tgt="content/post/forum/$id.md"
-   curl -sL https://noiopen.discourse.group/t/$id.json | jq -r -f autoblog.jq >$tgt
+   curl -sL "https://noiopen.discourse.group/t/$id.json" >$id.json
+   mkdir -p content/post/forum/$id
+   slug="$id/$(jq -r '.post_stream.posts[0].topic_slug' $id.json).md"
+   tgt="content/post/forum/$slug"
+   jq -r -f autoblog.jq <$id.json >$tgt
 done
 
